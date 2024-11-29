0841d930b9141180a83c7f7e849fd520205e6ded
Burnett Miao <searchfor1987@gmail.com>
2024-11-29 14:52:26 +0800

    banner section finish

--- script.js
+++ script.js
@@ -43,4 +43,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
     ...scrollRevealOption,
     delay: 1500,
   });
+
+  // banner section
+  const banner = document.querySelector('.banner__container');
+  const bannerContent = Array.from(banner.children);
+
+  bannerContent.forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute('aria-hidden', true);
+    banner.appendChild(duplicateNode);
+  });
 });
